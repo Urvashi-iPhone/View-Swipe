12161e729c4552f2f0b20f76393bcacb60a53a16
Tecksky Technologies <teckskyteam@gmail.com>
2016-10-13 17:57:41 +0530

    Initial Commit

---
+++ viewswipedemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  viewswipedemo
+//
+//  Created by Tecksky Techonologies on 10/13/16.
+//  Copyright © 2016 Softranz Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ viewswipedemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  viewswipedemo
+//
+//  Created by Tecksky Techonologies on 10/13/16.
+//  Copyright © 2016 Softranz Technologies. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
